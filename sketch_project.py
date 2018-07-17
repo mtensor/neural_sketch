b7a1ce8f9f013e67d09bd740385e5b465fdea3b4
Maxwell Nye <mnye@mit.edu>
2018-07-17 19:34:37 -0400

    Experiment holes_pretrain_2_correct_obj_1531870476048

--- sketch_project.py
+++ sketch_project.py
@@ -169,6 +169,7 @@ if __name__ == "__main__":
 
     if not hasattr(model, 'iteration'):
         model.iteration = 0
+    if not hasattr(model, 'iteration'):
         model.hole_scores = []
     for i in range(model.iteration, 10000):
 
