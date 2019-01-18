18805a3c54cf6cd29f591c876c780aee827d3381
Maxwell Nye <mnye@mit.edu>
2019-01-18 14:15:01 -0500

    Experiment algolisp_fullnew_1547838898823

--- train_and_test_full.sh
+++ train_and_test_full.sh
@@ -7,36 +7,36 @@ if [[ "$@" == "--inner" ]]; then
 	which python
 
 	#Only pretrain
-	# RES_PRE=$(sbatch --parsable -e 'pretrain.out' -o 'pretrain.out' execute_gpu.sh python train/main_supervised_algolisp.py --seed 43 --pretrain --max_epochs 0 --max_pretrain_epochs 45 --use_dataset_len 6000 --train_to_convergence)
-	# echo "pretraining job: $RES_PRE"
+	RES_PRE=$(sbatch --parsable -e 'pretrain.out' -o 'pretrain.out' execute_gpu.sh python train/main_supervised_algolisp.py --pretrain --max_epochs 0 --max_pretrain_epochs 45 --train_to_convergence)
+	echo "pretraining job: $RES_PRE"
 
 	# train dc_model:
 	RES_DC=$(sbatch --parsable -e 'dctrain.out' -o 'dctrain.out' execute_gpu.sh python train/algolisp_train_dc_model.py --max_epochs 25 --inv_temp 0.05 --nHoles 3 -k 50)
  	echo "dc model training job: $RES_DC"
 
 	# train model:
-	RES_TRAIN=$(sbatch --parsable --dependency=afterok:$RES_DC -e 'train.out' -o 'train.out' execute_gpu.sh python train/main_supervised_algolisp.py --load_pretrained_model_path ../algolisp_full_dataset_1544644404692/saved_models/algolisp_pretrained.p --train_to_convergence --max_epochs 45 --use_dc_grammar './saved_models/algolisp_dc_model.p' --inv_temp 0.25 --nHoles 3 -k 50 --use_timeout)
+	RES_TRAIN=$(sbatch --parsable --dependency=afterok:$RES_PRE -e 'train.out' -o 'train.out' execute_gpu.sh python train/main_supervised_algolisp.py --load_pretrained_model_path --train_to_convergence --max_epochs 45 --use_dc_grammar './saved_models/algolisp_dc_model.p' --inv_temp 0.25 --nHoles 3 -k 50 --use_timeout)
 
 	echo "eval jobs"
 	sbatch --dependency=afterok:$RES_TRAIN -e 'finaleval.out' -o 'finaleval.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --n_test 9967 --mdl 100 --queue --n_processes 44 --timeout 600 --max_to_check 20000 --resultsfile "results_model"
 
-	# sbatch --dependency=afterok:$RES_PRE -e 'finalevalrnn.out' -o 'finalevalrnn.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --n_test 9967 --mdl 100 --model_path "./saved_models/algolisp_pretrained.p" --resultsfile "results_rnn_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
+	sbatch --dependency=afterok:$RES_PRE -e 'finalevalrnn.out' -o 'finalevalrnn.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --n_test 9967 --mdl 100 --model_path "./saved_models/algolisp_pretrained.p" --resultsfile "results_rnn_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
 	
 	sbatch --dependency=afterok:$RES_DC -e 'finalevaldc.out' -o 'finalevaldc.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --n_test 9967 --mdl 100 --dc_baseline --resultsfile "results_dc_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
 
 	# echo "dev jobs:"
 	# # test model
-	# sbatch --dependency=afterok:$RES_TRAIN -e 'finaldev.out' -o 'finaldev.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --queue --n_processes 44 --timeout 600 --max_to_check 20000 --resultsfile "results_dev_model"
+	sbatch --dependency=afterok:$RES_TRAIN -e 'finaldev.out' -o 'finaldev.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --queue --n_processes 44 --timeout 600 --max_to_check 20000 --resultsfile "results_dev_model"
 
-	# sbatch --dependency=afterok:$RES_PRE -e 'finaldevrnn.out' -o 'finaldevrnn.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --model_path "./saved_models/algolisp_pretrained.p" --resultsfile "results_dev_rnn_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
+	sbatch --dependency=afterok:$RES_PRE -e 'finaldevrnn.out' -o 'finaldevrnn.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --model_path "./saved_models/algolisp_pretrained.p" --resultsfile "results_dev_rnn_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
 	
-	# sbatch --dependency=afterok:$RES_DC -e 'finaldevdc.out' -o 'finaldevdc.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --dc_baseline --resultsfile "results_dev_dc_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
+	sbatch --dependency=afterok:$RES_DC -e 'finaldevdc.out' -o 'finaldevdc.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --dataset 'dev' --n_test 10819 --mdl 100 --dc_baseline --resultsfile "results_dev_dc_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
 
 
 else
 	#to activate, should properly run:
 	echo "running main script at run.txt"
-	name=algolisp_full2 g-run bash train_and_test_full.sh --inner > run.txt & #can i do this??
+	name=algolisp_fullnew g-run bash train_and_test_full.sh --inner > run.txt & #can i do this??
 fi
 
 #sbatch -e 'evalrnn.out' -o 'evalrnn.out' execute_public_cpu.sh python eval/evaluate_algolisp.py --n_test 8995 --only_passable --model_path "./saved_models/algolisp_pretrained.p" --resultsfile "results_rnn_base" --queue --n_processes 44 --timeout 600 --max_to_check 20000
